cfcfede99c166ebc22b5a9ab17bc911cbb7d9697
sumanthreddyy <85357787+sumanthreddyy@users.noreply.github.com>
2023-06-09 19:36:07 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -17,9 +17,11 @@ def main():
     st.title("Real-time Voice Transcription")
 
     # Create a WebRTC microphone input
-    webrtc_ctx = webrtc.VideoTransformerBase(
-        transform_func=None,
-        fps=30,
+    webrtc_ctx = webrtc.webrtc_streamer(
+        key="microphone",
+        mode="audio",
+        audio_receiver_size=1024,
+        required=False,
     )
 
     # Flag to indicate if the microphone is active
@@ -39,16 +41,16 @@ def main():
                 st.info("Listening...")
 
                 # Get the audio frames from the WebRTC microphone input
-                for audio_frames in webrtc_ctx.generator():
-                    audio_data = b"".join([frame.to_ndarray() for frame in audio_frames])
-                    transcribe_speech(audio_data)
+                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=0.1)
+                audio_data = b"".join([frame.to_ndarray() for frame in audio_frames])
+                transcribe_speech(audio_data)
 
-                    # Check if Stop button is clicked
-                    if stop_button:
-                        is_transcribing = False
-                        stop_button = False  # Disable the Stop button
-                        st.warning("Transcription Stopped")
-                        break  # Exit the loop to stop transcription
+                # Check if Stop button is clicked
+                if stop_button:
+                    is_transcribing = False
+                    stop_button = False  # Disable the Stop button
+                    st.warning("Transcription Stopped")
+                    break  # Exit the loop to stop transcription
             else:
                 # Transcription already started
                 pass
@@ -66,20 +68,23 @@ def main():
             break  # Exit the loop to stop transcription
 
 def request_microphone_access():
-    webrtc_ctx = webrtc.VideoTransformerBase(
-        transform_func=None,
-        fps=30,
+    webrtc_ctx = webrtc.webrtc_streamer(
+        key="microphone",
+        mode="audio",
+        audio_receiver_size=1024,
+        required=True,
     )
-    webrtc_ctx.request_media_stream_constraints(audio=True)
 
 def initialize_app():
     st.title("Real-time Voice Transcription")
     st.sidebar.button("Grant Microphone Access", on_click=request_microphone_access)
 
 def check_microphone_access():
-    webrtc_ctx = webrtc.VideoTransformerBase(
-        transform_func=None,
-        fps=30,
+    webrtc_ctx = webrtc.webrtc_streamer(
+        key="microphone",
+        mode="audio",
+        audio_receiver_size=1024,
+        required=False,
     )
     return webrtc_ctx.state.playing
 
