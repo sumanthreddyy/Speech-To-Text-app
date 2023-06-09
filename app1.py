5002d5de390b93536db66548add3d50e9b0a9b37
sumanthreddyy <85357787+sumanthreddyy@users.noreply.github.com>
2023-06-09 19:00:22 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,84 +1,44 @@
 import streamlit as st
 import speech_recognition as sr
-import time
 
 def transcribe_speech(device_index):
     # Create a recognizer object
     r = sr.Recognizer()
 
-    # Flag to indicate if the microphone is active
-    is_transcribing = False
-    stop_transcription = False
+    # Use the selected microphone as the audio source
+    mic = sr.Microphone(device_index=device_index)
 
-    # Microphone button
-    start_button = st.sidebar.button("Start Transcription")
-    stop_button = st.sidebar.button("Stop Transcription")
-    t = 0
+    # Adjust the microphone for ambient noise
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
 
-    # Continuously transcribe audio input
-    while True:
-        try:
-            if start_button:
-                if not is_transcribing:
-                    # Start transcription
-                    is_transcribing = True
-                    stop_transcription = False
-                    start_button = False  # Disable the Start button
-                    st.info("Listening...")
+    # Start transcription
+    st.info("Listening...")
 
-                    # Use the client's microphone as the audio source
-                    with sr.Microphone(device_index=device_index) as source:
-                        r.adjust_for_ambient_noise(source)
-                        audio_data = r.listen(source)
+    with mic as source:
+        audio_data = r.listen(source)
 
-                    # Recognize speech using Google Speech Recognition
-                    text = r.recognize_google(audio_data)
+    # Recognize speech using Google Speech Recognition
+    text = r.recognize_google(audio_data)
 
-                    # Display the transcribed text in the Streamlit app
-                    if text.strip():  # Check if the text is not empty
-                        st.write(text)
+    # Display the transcribed text in the Streamlit app
+    if text.strip():  # Check if the text is not empty
+        st.write(text)
 
-                else:
-                    # Transcription already started
-                    pass
-
-            elif stop_button:
-                # Stop transcription
-                stop_transcription = True
-                is_transcribing = False
-                stop_button = False  # Disable the Stop button
-                st.warning("Transcription Stopped")
-                break  # Exit the loop to stop transcription
-
-            else:
-                while t < 1:
-                    st.info("Click the Transcription button again...")
-                    t += 1
-
-            if stop_transcription:
-                # Transcription stopped, break the loop
-                break
-
-            time.sleep(0.1)
-
-        except sr.UnknownValueError:
-            # Ignore any unrecognized speech
-            pass
 
 def main():
-    
     st.title("Real-time Voice Transcription")
-    print(sr.Microphone.list_microphone_names())
 
-    # Get the device index from the user
-    device_index = st.sidebar.number_input("Enter the input device index", value=0, step=1)
+    # Get the list of available microphones
+    microphone_list = sr.Microphone.list_microphone_names()
 
-    # Handle the case where the user enters 0 to represent default device index
-    if device_index == 0:
-        device_index = None
+    # Create a dropdown list for microphone selection
+    device_index = st.sidebar.selectbox("Select Microphone", microphone_list)
 
-    # Remove "Loading..." message once the app is loaded
-    transcribe_speech(device_index)
+    # Start transcription button
+    if st.sidebar.button("Start Transcription"):
+        transcribe_speech(device_index)
+
 
 if __name__ == "__main__":
     main()
