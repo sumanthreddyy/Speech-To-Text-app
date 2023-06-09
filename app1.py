1f570558d68cdafb36df50528b51b1d0194dcf09
sumanthreddyy <85357787+sumanthreddyy@users.noreply.github.com>
2023-06-09 19:32:01 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,87 +1,84 @@
 import streamlit as st
+import streamlit_webrtc as webrtc
 import speech_recognition as sr
-import time
 
-def transcribe_speech():
+def transcribe_speech(audio_data):
     # Create a recognizer object
     r = sr.Recognizer()
 
-    # Use the default microphone as the audio source
-    mic = None
+    # Recognize speech using Google Speech Recognition
+    text = r.recognize_google(audio_data)
 
-    try:
-        mic = sr.Microphone()
-        with mic as source:
-            r.adjust_for_ambient_noise(source)
-    except sr.RequestError:
-        st.error("Could not access the microphone. Please make sure you have a microphone connected.")
-        return
-    except sr.UnknownValueError:
-        st.error("An unknown error occurred while accessing the microphone.")
-        return
-
-    # Flag to indicate if the microphone is active
-    is_transcribing = False
-    stop_transcription = False
-
-    # Microphone button
-    start_button = st.sidebar.button("Start Transcription")
-    stop_button = st.sidebar.button("Stop Transcription")
-    t = 0
-
-    # Continuously transcribe audio input
-    while True:
-        try:
-            if start_button:
-                if not is_transcribing:
-                    # Start transcription
-                    is_transcribing = True
-                    stop_transcription = False
-                    start_button = False  # Disable the Start button
-                    st.info("Listening...")
-
-                    with mic as source:
-                        audio_data = r.listen(source)
-
-                    # Recognize speech using Google Speech Recognition
-                    text = r.recognize_google(audio_data)
-
-                    # Display the transcribed text in the Streamlit app
-                    if text.strip():  # Check if the text is not empty
-                        st.write(text)
-
-                else:
-                    # Transcription already started
-                    pass
-
-            elif stop_button:
-                # Stop transcription
-                stop_transcription = True
-                is_transcribing = False
-                stop_button = False  # Disable the Stop button
-                st.warning("Transcription Stopped")
-                break  # Exit the loop to stop transcription
-
-            else:
-                while t < 1:
-                    st.info("Click the Transcription button again...")
-                    t += 1
-
-            if stop_transcription:
-                # Transcription stopped, break the loop
-                break
-
-            time.sleep(0.1)
-
-        except sr.UnknownValueError:
-            # Ignore any unrecognized speech
-            pass
+    # Display the transcribed text in the Streamlit app
+    if text.strip():  # Check if the text is not empty
+        st.write(text)
 
 def main():
     st.title("Real-time Voice Transcription")
 
-    # Remove "Loading..." message once the app is loaded
-    transcribe_speech()
+    # Create a WebRTC microphone input
+    webrtc_ctx = webrtc.StreamlitWebRTC()
+
+    # Flag to indicate if the microphone is active
+    is_transcribing = False
+
+    # Microphone button
+    start_button = st.sidebar.button("Start Transcription")
+    stop_button = st.sidebar.button("Stop Transcription")
+
+    # Continuously transcribe audio input
+    while True:
+        if start_button:
+            if not is_transcribing:
+                # Start transcription
+                is_transcribing = True
+                start_button = False  # Disable the Start button
+                st.info("Listening...")
+
+                # Create a WebRTC audio processor
+                audio_processor = webrtc_ctx._create_audio_processor()
+
+                # Get the audio frames from the WebRTC audio processor
+                for audio_frames in audio_processor.generator():
+                    audio_data = b"".join([frame.to_ndarray() for frame in audio_frames])
+                    transcribe_speech(audio_data)
+
+                    # Check if Stop button is clicked
+                    if stop_button:
+                        is_transcribing = False
+                        stop_button = False  # Disable the Stop button
+                        st.warning("Transcription Stopped")
+                        break  # Exit the loop to stop transcription
+            else:
+                # Transcription already started
+                pass
+        else:
+            if not is_transcribing:
+                st.info("Click the Start Transcription button...")
+            else:
+                st.info("Transcription in progress...")
+
+        if stop_button:
+            # Stop transcription
+            is_transcribing = False
+            stop_button = False  # Disable the Stop button
+            st.warning("Transcription Stopped")
+            break  # Exit the loop to stop transcription
+
+def request_microphone_access():
+    webrtc_ctx = webrtc.StreamlitWebRTC()
+    webrtc_ctx.request_media_stream_constraints(audio=True)
+
+def initialize_app():
+    st.title("Real-time Voice Transcription")
+    st.sidebar.button("Grant Microphone Access", on_click=request_microphone_access)
+
+def check_microphone_access():
+    webrtc_ctx = webrtc.StreamlitWebRTC()
+    return webrtc_ctx.state.playing
 
 if __name__ == "__main__":
-    main()
+    if check_microphone_access():
+        main()
+    else:
+        initialize_app()
